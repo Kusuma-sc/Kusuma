63d20643a6ac72e395a829447486c2e367861531
Kusuma <varshibalaji76@gmail.com>
2026-03-24 20:47:45 +0530

    fix templates folder

--- app.py
+++ app.py
@@ -1,57 +1,7 @@
-from flask import Flask, render_template, request, session
-from flask_socketio import SocketIO, emit
+from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = "secret123"
-
-# ✅ IMPORTANT: use threading instead of eventlet
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
-
-users = {}
 
 @app.route("/")
-def login():
-    return render_template("login.html")
-
-@app.route("/chat", methods=["POST"])
-def chat():
-    email = request.form.get("email")
-
-    if not email:
-        return "Email required"
-
-    session["email"] = email
-    return render_template("chat.html", email=email)
-
-# ===== SOCKET =====
-@socketio.on("connect")
-def connect():
-    print("Client connected")
-
-@socketio.on("register")
-def register(data):
-    email = data.get("email")
-    if email:
-        users[email] = request.sid
-        print("Registered:", email)
-
-@socketio.on("call_user")
-def call_user(data):
-    target = data["target"]
-    if target in users:
-        emit("incoming_call", {"from": data["from"]}, to=users[target])
-
-@socketio.on("webrtc_offer")
-def offer(data):
-    if data["to"] in users:
-        emit("webrtc_offer", data, to=users[data["to"]])
-
-@socketio.on("webrtc_answer")
-def answer(data):
-    if data["to"] in users:
-        emit("webrtc_answer", data, to=users[data["to"]])
-
-@socketio.on("ice_candidate")
-def ice(data):
-    if data["to"] in users:
-        emit("ice_candidate", data, to=users[data["to"]])
+def home():
+    return "Server Working"
