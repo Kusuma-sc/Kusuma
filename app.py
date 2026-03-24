32a3f41949ac7f409d025a40127464056e3e1587
Kusuma <varshibalaji76@gmail.com>
2026-03-24 21:50:32 +0530

    fix socketio deploy

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def ice(data):
 
 # ---------- RUN ----------
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
+    if __name__ == "__main__":
+        socketio.run(app)
